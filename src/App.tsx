aed781aefaa3e28184b0c75424a84a0f28568e8a
anten <chch4lee1@naver.com>
2023-02-23 15:53:29 +0900

    fix: kakao 관련 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./index.css";
-import { Main } from "./pages";
-import KakaoShareButton from "./components/KakoShareButton";
-import Dragtest from "./pages/Dragtest";
-import AddDnd from "./pages/AddDnd";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
+import { Main } from './pages';
+import Dragtest from './pages/Dragtest';
+import AddDnd from './pages/AddDnd';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/KakaoShareButton" element={<KakaoShareButton />} />
-      <Route path="/DragTest" element={<Dragtest />} />
-      <Route path="/AddDnd" element={<AddDnd />} />
-    </Routes>
-  );
+   return (
+      <Routes>
+         <Route path="/" element={<Main />} />
+         <Route path="/DragTest" element={<Dragtest />} />
+         <Route path="/AddDnd" element={<AddDnd />} />
+      </Routes>
+   );
 }
 
 export default App;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,25 +1,21 @@
-import { Button } from "@mui/material";
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Button } from '@mui/material';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Main = () => {
-  const navigate = useNavigate();
-  const sharebutton = () => {
-    navigate("/KakaoShareButton");
-  };
-  const dragbutton = () => {
-    navigate("/DragTest");
-  };
-  const adddndbutton = () => {
-    navigate("/AddDnd");
-  };
-  return (
-    <>
-      <h1>Hello World!</h1>
-      <Button onClick={sharebutton}>공유하기</Button>
-      <Button onClick={dragbutton}>드래그 테스트</Button>
-      <Button onClick={adddndbutton}>추가 드래그 테스트</Button>
-    </>
-  );
+   const navigate = useNavigate();
+   const dragbutton = () => {
+      navigate('/DragTest');
+   };
+   const adddndbutton = () => {
+      navigate('/AddDnd');
+   };
+   return (
+      <>
+         <h1>Hello World!</h1>
+         <Button onClick={dragbutton}>드래그 테스트</Button>
+         <Button onClick={adddndbutton}>추가 드래그 테스트</Button>
+      </>
+   );
 };
 export default Main;
