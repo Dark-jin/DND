405bcf1b66fd1fafd246e2d0b98b9f0e9274fc39
Dark-jin <chch4lee1@naver.com>
2023-03-01 15:02:10 +0900

    fix: ImageUpload css 수정(드래그드롭해도 이미지가 들어가짐!)

--- src/pages/ImageUpload.tsx
+++ src/pages/ImageUpload.tsx
@@ -1,30 +1,34 @@
-import { Button } from '@mui/material';
-import React, { useState } from 'react';
+import { Button } from "@mui/material";
+import React, { useState } from "react";
 
 const ImageUpload = () => {
-   const [fileImage, setFileImage] = useState('');
-   const saveFileImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-      // @ts-ignore
-      setFileImage(URL.createObjectURL(event.target.files[0]));
-   };
-   const deleteImage = () => {
-      URL.revokeObjectURL(fileImage);
-      setFileImage('');
-   };
-   return (
+  const [fileImage, setFileImage] = useState("");
+  const saveFileImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // @ts-ignore
+    setFileImage(URL.createObjectURL(event.target.files[0]));
+  };
+  const deleteImage = () => {
+    URL.revokeObjectURL(fileImage);
+    setFileImage("");
+  };
+  return (
+    <div className="text-center mt-10">
+      <input
+        type="file"
+        className="file-input file-input-bordered w-full max-w-xs"
+        accept="image/*"
+        onChange={saveFileImage}
+      />
+      <h2>이미지 보기</h2>
       <div>
-         <input
-            type="file"
-            className="file-input file-input-bordered w-full max-w-xs"
-            accept="image/*"
-            onChange={saveFileImage}
-         />
-         <h2>이미지 보기</h2>
-         <div>{fileImage && <img alt="sample" src={fileImage} style={{ margin: 'auto' }} />}</div>
-         <button className="btn" onClick={deleteImage}>
-            삭제
-         </button>
+        {fileImage && (
+          <img alt="sample" src={fileImage} style={{ margin: "auto" }} />
+        )}
       </div>
-   );
+      <button className="btn" onClick={deleteImage}>
+        삭제
+      </button>
+    </div>
+  );
 };
 export default ImageUpload;
